31a3045ff1a135646dc0293bad0a22fd8567ce89
kurt.iamele <kurt.iamele@gmail.com>
2020-05-07 19:19:34 -0500

    fixed venue list bug

--- script2.js
+++ script2.js
@@ -9,18 +9,27 @@ $(document).ready(function () {
         async: false,
         dataType: "json",
         success: function (data) {
-            autoList = [];
+            autoList = {};
+            autoList2 = [];
             p0 = '<option value="-1">Select Venue</option>';
             for (var i = 0; i < data._embedded.venues.length; i++) {
                 p0 += '<option value="' + data._embedded.venues[i].name + '">' + data._embedded.venues[i].name + '</option>';
-                autoList += data._embedded.venues[i].name + "<br>"
+                // autoList += JSON.stringify(data._embedded.venues[i].name)
                 $("#myList").html(p0)
-
                 // $('#venue-view').html(autoList)
+                // autoList = data._embedded.venues[i].name
+                // autoList2 += autoList.name
+                // console.log(autoList)
+                $("input#search-textbox").autocomplete({
+                    // source: [autoList]
+                });
+
             }
         }
-        // 2nd api call for venues over 200 teh append to #mylist selection in index.
+
+        // 2nd api call for venues over 200 then append to #mylist selection in index.
     });
+
     $.ajax({
         type: "GET",
         url: "https://app.ticketmaster.com/discovery/v2/venues.json?&latlong=32.79067196,-96.81081803&dmaId=261&apikey=IbUXFudvQmR2gZo5kLDiuVkaZTavZiEV&sort=name,asc&radius=25&unit=miles&size=200&page=1",
@@ -28,142 +37,85 @@ $(document).ready(function () {
         timeout: 30000,
         dataType: "json",
         success: function (data) {
-            var autoLista = [];
+            autoLista = [];
             var p1 = '<option value="-1"></option>';
             for (var i = 0; i < data._embedded.venues.length; i++) {
                 p1 += '<option value="' + data._embedded.venues[i].name + '">' + data._embedded.venues[i].name + '</option>';
-                autoLista += data._embedded.venues[i].name + "<br>"
+                autoList += JSON.stringify(data._embedded.venues[i].name)
+                $("#myList").html(p0)
                 $(p1).appendTo("#myList");
-
             }
 
         }
 
-    });
-
-    $("#myList").on("change", function (event) {
-
-        event.preventDefault();
-
-
-
-        // var longitudeNumber = ;
-        // var latitudeNumber = ;
-
-
-        var ZOMATO_APIKEY = "85e3651bab1da1f9c3235bf3ef189884";
-        var ZOMATO_URL = "https://developers.zomato.com/api/v2.1/search?count=20&lat=32.7767&lon=-96.803398&radius=5&sort=real_distance&order=asc&apikey=" + ZOMATO_APIKEY;
-
-        $.ajax({
-            url: ZOMATO_URL,
-            method: "GET",
-            async: false,
-            dataType: "json",
-            success: function (data) {
-                var autoList = [];
-                var p3 = '<option value="-1">Select Food Type</option>';
-
-                for (var i = 0; i < data.restaurants.length; i++) {
-                    p3 += '<option value="' + data.restaurants[i].restaurant.cuisines + '">' + data.restaurants[i].restaurant.cuisines + '</option>';
-                    autoList += data.restaurants[i].restaurant.cuisines + "<br>"
-                    $("#sel_user").html(p3)
-
-
-                }
-            }
-
-        });
-
-        place = $("#myList option:selected").text()
-
-
-        var unLocURL = "https://app.ticketmaster.com/discovery/v2/suggest?apikey=IbUXFudvQmR2gZo5kLDiuVkaZTavZiEV&keyword=" + place + "&locale=*";
-        var enLocURL = encodeURI(unLocURL);
-        console.log(enLocURL);
-
-        $(document).ready(function () {
-            $.ajax({
-                type: "GET",
-                url: enLocURL,
-                data: "{}",
-                success: function (data) {
-                    loc = data._embedded.venues[0].location;
-                    console.log("Venue: " + place);
-                    console.log("Latitude: " + loc.latitude);
-                    console.log("Longitude: " + loc.longitude);
-
-                    // $("#location").html("<p>" + place + "<br>" + loc.latitude + "<br>" + loc.longitude + "</p>")
-                }
-            });
-
-        });
 
     });
-
-    // var longitudeNumber = ;
-    // var latitudeNumber = ;
-    $("#myList").on("change", function (event) {
-
-        var ZOMATO_APIKEY = "85e3651bab1da1f9c3235bf3ef189884";
-        var ZOMATO_URL = "https://developers.zomato.com/api/v2.1/search?count=20&lat=32.7767&lon=-96.803398&radius=5&sort=real_distance&order=asc&apikey=" + ZOMATO_APIKEY;
-
-        $.ajax({
-            url: ZOMATO_URL,
-            method: "GET",
-            async: false,
-            dataType: "json",
-            success: function (data) {
-                var autoList = [];
-                var p3 = '<option value="-1">Select Food Type</option>';
-
-                for (var i = 0; i < data.restaurants.length; i++) {
-                    p3 += '<option value="' + data.restaurants[i].restaurant.cuisines + '">' + data.restaurants[i].restaurant.cuisines + '</option>';
-                    autoList += data.restaurants[i].restaurant.cuisines + "<br>"
-                    $("#sel_user").html(p3)
-
-
-                }
-            }
-        });
-
-
-        // will find the above selected venues location (lat and long) and store in variables: loc.latitude and loc.longitude
-        $("#find-venue").on("click", function (event) {
-
-            event.preventDefault();
-
-            place = $("#myList option:selected").text()
-
-
-            var unLocURL = "https://app.ticketmaster.com/discovery/v2/suggest?apikey=IbUXFudvQmR2gZo5kLDiuVkaZTavZiEV&keyword=" + place + "&locale=*";
-            var enLocURL = encodeURI(unLocURL);
-            console.log(enLocURL);
-
-            $(document).ready(function () {
-                $.ajax({
-                    type: "GET",
-                    url: enLocURL,
-                    data: "{}",
-                    success: function (data) {
-                        var loc = data._embedded.venues[0].location;
-                        console.log("Venue: " + place);
-                        console.log("Latitude: " + loc.latitude);
-                        console.log("Longitude: " + loc.longitude);
-
-                        $("#location").html("<p>" + place + "<br>" + loc.latitude + "<br>" + loc.longitude + "</p>")
-                    }
-                });
-
-            });
-
-        });
-    });
-
-
-    // will find the above selected venues location (lat and long) and store in variables: loc.latitude and loc.longitude
-
 
 });
+
+// will find the above selected venues location (lat and long) and store in variables: loc.latitude and loc.longitude
+$("#sel_user").on("change", function (event) {
+
+    event.preventDefault();
+
+    place = $("#myList option:selected").text()
+
+
+    var unLocURL = "https://app.ticketmaster.com/discovery/v2/suggest?apikey=IbUXFudvQmR2gZo5kLDiuVkaZTavZiEV&keyword=" + place + "&locale=*";
+    var enLocURL = encodeURI(unLocURL);
+    console.log(enLocURL);
+
+    $(document).ready(function () {
+        $.ajax({
+            type: "GET",
+            url: enLocURL,
+            data: "{}",
+            success: function (data) {
+                loc = data._embedded.venues[0].location
+                // console.log("Venue: " + place);
+                // console.log("Latitude: " + loc.latitude);
+                // console.log("Longitude: " + loc.longitude);
+                // $("#location").html("<p>" + place + "<br>" + loc.latitude + "<br>" + loc.longitude + "</p>")
+            }
+        });
+
+    });
+
+});
+
+
+
+
+
+
+$("#myList").on("change", function (event) {
+
+    event.preventDefault();
+
+
+    var ZOMATO_APIKEY = "85e3651bab1da1f9c3235bf3ef189884";
+    var ZOMATO_URL = "https://developers.zomato.com/api/v2.1/search?count=20&lat=32.7767&lon=-96.803398&radius=5&sort=real_distance&order=asc&apikey=" + ZOMATO_APIKEY;
+
+    $.ajax({
+        url: ZOMATO_URL,
+        method: "GET",
+        async: false,
+        dataType: "json",
+        success: function (data) {
+            var autoList = [];
+            var p3 = '<option value="-1">Select Food Type</option>';
+
+            for (var i = 0; i < data.restaurants.length; i++) {
+                p3 += '<option value="' + data.restaurants[i].restaurant.cuisines + '">' + data.restaurants[i].restaurant.cuisines + '</option>';
+                autoList += data.restaurants[i].restaurant.cuisines + "<br>"
+                $("#sel_user").html(p3)
+
+
+            }
+        }
+    });
+});
+
 
 $("#find-venue").on("click", function (event) {
 
@@ -173,7 +125,9 @@ $("#find-venue").on("click", function (event) {
     var foodType = $('#sel_user option:selected').text()
 
     $("#location").html("<p>" + place + "<br>" + loc.latitude + "<br>" + loc.longitude + "<br>" + foodType + "</p>");
-
+    console.log("Venue: " + place);
+    console.log("Latitude: " + loc.latitude);
+    console.log("Longitude: " + loc.longitude);
     console.log(foodType);
 
 });
